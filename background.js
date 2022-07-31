5943c771af409287b59be053a8190d571689e17c
prism-cube <ocean5014dev@yahoo.co.jp>
2022-07-31 23:44:22 +0900

    padding修正

--- background.js
+++ background.js
@@ -10,6 +10,8 @@ const toggleSideBar = () => {
     main.parentNode.style.padding = '0';
     main.firstElementChild.style.maxWidth = '100%';
     main.firstElementChild.style.maxHeight = '100%';
+    main.lastElementChild.style.paddingLeft = '32px';
+    main.lastElementChild.style.paddingRight = '32px';
     sideBar.style.display = 'none';
 
     main.dataset.visible = 'false';
@@ -18,6 +20,8 @@ const toggleSideBar = () => {
     main.parentNode.style.padding = '24px 32px 0 32px';
     main.firstElementChild.style.maxWidth = 'calc(100vw - 463px)';
     main.firstElementChild.style.maxHeight = 'calc(56.25vw - 260.4375px)';
+    main.lastElementChild.style.paddingLeft = '0';
+    main.lastElementChild.style.paddingRight = '0';
     sideBar.style.display = 'flex';
 
     main.dataset.visible = 'true';
